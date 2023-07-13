624f5e9a3dfdf87b44ee846c14164b4b02e925dd
jwkriewallLW <john.kriewall@lightwellinc.com>
2023-07-13 14:11:09 -0400

    Added user createdOn / By

---
+++ src/main/java/com/example/matchuptracker/model/User.java
@@ -0,0 +1,10 @@
+package com.example.matchuptracker.model;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+    private String username;
+    private Date accountCreatedOn;
+}
