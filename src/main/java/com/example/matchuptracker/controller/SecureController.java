58ff12cd277f3ec7742e75f4c15b1a5e8a5c5af2
jw-kriewall <jwkriewall@gmail.com>
2023-07-25 14:59:24 -0400

    Added user oauth2 google to backend

---
+++ src/main/java/com/example/matchuptracker/controller/SecureController.java
@@ -0,0 +1,19 @@
+package com.example.matchuptracker.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/secure")
+@Slf4j
+@CrossOrigin
+public class SecureController {
+
+    @GetMapping
+    public String secureEndpoint() {
+        return "Secure endpoint";
+    }
+}
