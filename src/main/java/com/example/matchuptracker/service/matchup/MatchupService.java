f84a454dba4ef29e20a7106df21e25e17ba0e195
jw-kriewall <jwkriewall@gmail.com>
2024-03-18 15:41:50 -0400

    Pushing change

--- src/main/java/com/example/matchuptracker/service/matchup/MatchupService.java
+++ src/main/java/com/example/matchuptracker/service/matchup/MatchupService.java
@@ -20,7 +20,7 @@ public interface MatchupService {
 //    Map<String, String> getIndividualRecordsByDeckName(String deckName);
     Map<String, Integer> getTotalMatchesByDeck(String email, String deckName);
     Map<String, Map<String, String>> getAllMatchupRecords(String email, String format);
-    void deleteMatchup(int id);
+    void deleteMatchup(int id, String email);
     Integer countAllByPlayerEmail(String email, List<String> deckNames);
     // @TODO: Get matchups by UserID - modify getAllMatchupsByPlayerName to be by playerID
     List<Matchup> getAllMatchupsByPlayerEmailAndFormat(String email, String format);
