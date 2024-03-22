7c6230f8dc81f5b8f10490c4bd3d2ad62905fbd5
jw-kriewall <jwkriewall@gmail.com>
2024-03-22 18:50:41 -0400

    Pushing progress

--- src/main/java/com/example/matchuptracker/controller/OAuthController.java
+++ src/main/java/com/example/matchuptracker/controller/OAuthController.java
@@ -1,10 +1,16 @@
-package com.example.matchuptracker.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class OAuthController {
-
+//package com.example.matchuptracker.controller;
+//
+//import com.example.matchuptracker.service.oauth.GoogleAuthService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.Map;
+//
+//@RestController
+//public class OAuthController {
+//
 //    @Autowired
 //    private GoogleAuthService googleAuthService;
 //
@@ -13,4 +19,4 @@ public class OAuthController {
 //        String authorizationCode = payload.get("code");
 //        return googleAuthService.exchangeCodeForTokens(authorizationCode);
 //    }
-}
+//}
--- src/main/java/com/example/matchuptracker/service/oauth/GoogleAuthService.java
+++ src/main/java/com/example/matchuptracker/service/oauth/GoogleAuthService.java
@@ -1,37 +1,44 @@
-package com.example.matchuptracker.service.oauth;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import java.util.HashMap;
-import java.util.Map;
-
-@Service
-public class GoogleAuthService {
-
-    @Value("${GOOGLE_CLIENT_ID}")
-    private String clientId;
-
-    @Value("${GOOGLE_CLIENT_SECRET}")
-    private String clientSecret;
-
-    @Value("${GOOGLE_REDIRECT_URI}")
-    private String redirectUri;
-
-    public Map<String, Object> exchangeCodeForTokens(String authorizationCode) {
-        RestTemplate restTemplate = new RestTemplate();
-        String tokenEndpoint = "https://oauth2.googleapis.com/token";
-
-        Map<String, String> requestBody = new HashMap<>();
-        requestBody.put("code", authorizationCode);
-        requestBody.put("client_id", clientId);
-        requestBody.put("client_secret", clientSecret);
-        requestBody.put("redirect_uri", redirectUri);
-        requestBody.put("grant_type", "authorization_code");
-
-        @SuppressWarnings("unchecked")
-        Map<String, Object> response = restTemplate.postForObject(tokenEndpoint, requestBody, Map.class);
-
-        return response;
-    }
-}
+//package com.example.matchuptracker.service.oauth;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.client.HttpClientErrorException;
+//import org.springframework.web.client.RestTemplate;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Service
+//public class GoogleAuthService {
+//
+//    @Value("${GOOGLE_CLIENT_ID}")
+//    private String clientId;
+//
+//    @Value("${GOOGLE_CLIENT_SECRET}")
+//    private String clientSecret;
+//
+//    @Value("${GOOGLE_REDIRECT_URI}")
+//    private String redirectUri;
+//
+//    public Map<String, Object> exchangeCodeForTokens(String authorizationCode) {
+//        RestTemplate restTemplate = new RestTemplate();
+//        String tokenEndpoint = "https://oauth2.googleapis.com/token";
+//
+//        Map<String, String> requestBody = new HashMap<>();
+//        requestBody.put("client_id", clientId);
+//        requestBody.put("client_secret", clientSecret);
+////        requestBody.put("code", authorizationCode);
+//        requestBody.put("refresh_token", authorizationCode);
+//        requestBody.put("grant_type", "refresh_token");
+//
+//        try {
+//            @SuppressWarnings("unchecked")
+//            Map<String, Object> response = restTemplate.postForObject(tokenEndpoint, requestBody, Map.class);
+//            return response;
+//        } catch (HttpClientErrorException e) {
+//            // Log or process the error response
+//            System.out.println(e.getResponseBodyAsString());
+//            // Return an appropriate error response or rethrow as a custom exception
+//            throw e;
+//        }
+//    }
+//}
